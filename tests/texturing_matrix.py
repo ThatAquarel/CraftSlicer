bf37dde59451df3e825a45e4ebf91ac4e6dca53b
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2021-06-30 15:40:40 -0400

    Remove tests

--- tests/texturing_matrix.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from core.gl.gl_processor import position_matrix
-
-position_matrix_90 = np.load("position_matrix_90.npy")
-position_matrix_0 = position_matrix([0, 0, 0], [0, 0, 0], [1, 1, 1], reverse=True)
-print("")
